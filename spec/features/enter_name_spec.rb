6b57bbbbfac8bf2b564cda946374302e0a42efdb
Eleanor Chamberlain-Stoltzfus <eleanor.stoltzfus@gmail.com>
2022-03-22 15:07:29 +0000

    updated gemfile and added new specs and views

---
+++ spec/features/enter_name_spec.rb
@@ -0,0 +1,14 @@
+# in spec/features/enter_names_spec.rb
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Charlotte'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+
+    save_and_open_page
+    
+    expect(page).to have_content 'Charlotte vs. Mittens'
+  end
+end
