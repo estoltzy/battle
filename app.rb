7376fbc2c66b479000a8cd648fee08ca751308a3
Eleanor Chamberlain-Stoltzfus <eleanor.stoltzfus@gmail.com>
2022-03-22 15:22:15 +0000

    refactored POST request

--- app.rb
+++ app.rb
@@ -4,18 +4,22 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-  end
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
-  post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
+  end
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end
 
   # start the server if ruby file executed directly
