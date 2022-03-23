fd06dead471b67b2babcb28ac1036969a8e51617
Eleanor Chamberlain-Stoltzfus <eleanor.stoltzfus@gmail.com>
2022-03-23 13:00:52 +0000

    added web helpers file

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -2,10 +2,7 @@
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Charlotte'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
 
     save_and_open_page
     
