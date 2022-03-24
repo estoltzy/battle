c4f4f9a8ceb8851a0a23c7eeba62ebec910e17d1
Eleanor Chamberlain-Stoltzfus <eleanor.stoltzfus@gmail.com>
2022-03-24 11:50:49 +0000

    updated player class and spec

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,8 +1,12 @@
-feature 'player attacks' do
-  scenario 'player 1 attacks player 2' do
+# in spec/features/attack_spec.rb
+
+feature 'Attacking' do
+  # 'attack Player 2' scenario omitted
+
+  scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-
-    expect(page).to have_content 'Charlotte attacked Mittens'
+    expect(page).not_to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,4 +1,4 @@
-feature 'View hit points' do
+xfeature 'View hit points' do
   scenario 'see Player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content 'Mittens: 60HP'
